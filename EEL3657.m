7695fc8e75a298b8991b0ddc9f9d088a8d5d0c39
Chris Orem <chrisorem@gmail.com>
2015-07-23 11:03:30 -0400

    add images

--- EEL3657.m
+++ EEL3657.m
@@ -23,16 +23,18 @@ Gc = Kc*(s+1/Tg)/(s+1/(B*Tg)); % = 1 initially
 GH = (0.2*s +3.2)/((s+1)*(s+.8));
 
 % find Kv
-syms x;
+syms x b;
 P = Kp + Ki/x + Kd*x;
-C= (x+1/Tl)/(x+1/(B*Tl));
+C = (x+1/Tg)/(x+1/(B*Tg));
 Gh = (0.2*x +3.2)/((x+1)*(x+.8));
+c = (x+1/Tg)/(x+1/(b*Tg));
+% solve(limit((x*c*P*Gh),0),b)
 Kv = limit((x*C*P*Gh),0);
 fprintf('The value of Kv is %s\n',char(Kv));
 % show root locus
-%figure() 
-%rlocus(GH)
-%sgrid
+figure() 
+rlocus(GH)
+sgrid
 
 figure()
 sys = feedback(K * Gc * GH,1);
