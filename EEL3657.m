1bd1a6b5ac114fdd7d3f121aadbab9125bb538ee
Chris Orem <chrisorem@gmail.com>
2015-07-23 13:48:18 -0400

    3

--- EEL3657.m
+++ EEL3657.m
@@ -10,41 +10,50 @@ Kp = 1;
 Kd = 0;
 Ki = 0;
 K = pid(Kp,Ki,Kd);
+k=tf(K);
 
 %setup a lag compensator
 Kc = 1;  % choose Kc = 1
 Tl = 1;
 Tg = 0.05;
 B = 1; % 1.25;
+A = 1;
 s=tf('s');
 
-Gc = Kc*(s+1/Tg)/(s+1/(B*Tg)); % = 1 initially
+Gc = (s+1/Tg)/(s+1/(B*Tg)); % = 1 initially lag
+
+Gl = (s+(1*A)/Tl)/(s+1/(Tl)); % = 1 initially lead
 
 GH = (0.2*s +3.2)/((s+1)*(s+.8));
 
 % find Kv
 syms x b;
 P = Kp + Ki/x + Kd*x;
-C = (x+1/Tg)/(x+1/(B*Tg));
+Cg = (x+1/Tg)/(x+1/(B*Tg));
+Cl = (x+(1*A)/Tl)/(x+1/(Tl));
 Gh = (0.2*x +3.2)/((x+1)*(x+.8));
 c = (x+1/Tg)/(x+1/(b*Tg));
 % solve(limit((x*c*P*Gh),0),b)
-Kv = limit((x*C*P*Gh),0);
+Kv = limit((x * Cg * Cl * P * Gh),0);
 fprintf('The value of Kv is %s\n',char(Kv));
+
+
 % show root locus
 figure() 
+rlocus(k*Kc*Gc*Gl*GH)
+hold on
 rlocus(GH)
 sgrid
-
+hold off
 figure()
-sys = feedback(K * Gc * GH,1);
+sys = feedback(k * Kc * Gc * Gl * GH,1);
 
 % show unit step plot
-subplot(211), step(sys)
+step(sys)
 t = 0:.01:100;
-
+figure()
 % show unit ramp plot
-subplot(212), lsim(sys,t,t)
+lsim(sys,t,t)
 title('Response to Unit Ramp Input')
 sse = abs(1-dcgain(sys));
 fprintf('The sse is %f\n',sse);
