1862fda1990ab322fc9748ab107f6f06fa868ead
Chris Orem <chrisorem@gmail.com>
2015-07-24 18:46:54 -0400

    6

--- EEL3657.m
+++ EEL3657.m
@@ -6,56 +6,59 @@
 %Ki              Decrease        Increase   Increase       Eliminate
 %Kd              Small Change    Decrease   Decrease       No Change
 
+clc
+
 Kp = 1;
-Kd = 1;
-Ki = 1;
+Kd = 0;
+Ki = 0;
+
+Kc = 1;     % choose Kc = 1
+Tl = 1;     % T for lead compensation
+Tg = 0.05;  % T for lag compensation
+A = 1;      % alpha 
+
+s=tf('s');
+
+
+% setup the PID control
 K = pid(Kp,Ki,Kd);
-k=tf(K);
+P=tf(K);
 
 
 % find Kv
 syms x b;
-P = Kp + Ki/x + Kd*x;           %pid 
-Cl = (x+(1*A)/Tl)/(x+1/(Tl));    % lead
-Gh = (0.2*x +3.2)/((x+1)*(x+.8));  % plant
-c = (x+1/Tg)/(x+1/(b*Tg));   % lag
-kv = limit((x*c*P*Gh),0);
+Px = Kp + Ki/x + Kd*x;              %pid 
+GHx = (0.2*x +3.2)/((x+1)*(x+.8));  % plant
+Cg = (x+1/Tg)/(x+1/(b*Tg));         % lag
+
+kv = limit((x*Cg*Px*GHx),0);
 % set Beta to that value
 B = double(solve(kv==5,b));
 
+
+
 %setup a lag compensator
-Kc = 1;  % choose Kc = 1
-Tl = 1;
-Tg = 0.05;
-%B = 1.25; % 1.25;
-A = 1;
-s=tf('s');
 
 Gc = (s+1/Tg)/(s+1/(B*Tg)); % = 1 initially (lag)
-
 Gl = (s+(1*A)/Tl)/(s+1/(Tl)); % = 1 initially (lead)
+GHs = (0.2*s +3.2)/((s+1)*(s+.8));
 
-GH = (0.2*s +3.2)/((s+1)*(s+.8));
-
-
-P = Kp + Ki/x + Kd*x;           %pid
 Cg = (x+1/Tg)/(x+1/(B*Tg));     
 Cl = (x+(1*A)/Tl)/(x+1/(Tl));    % lead
-Gh = (0.2*x +3.2)/((x+1)*(x+.8));  % plant
-c = (x+1/Tg)/(x+1/(b*Tg));   % lag
-limit((x*c*P*Gh),0)
-Kv = limit((x * Cg * Cl * P * Gh),x,0);
+
+limit((x*Cg*Px*GHx),0)
+Kv = limit((x * Cg * Cl * Px * GHx),x,0);
 fprintf('The value of Kv is %s\n',char(Kv));
 
 % show root locus
 figure() 
-rlocus(k*Kc*Gc*Gl*GH)
+rlocus(P*Kc*Gc*Gl*GHs)
 hold on
-rlocus(GH)
+rlocus(GHs)
 sgrid
 hold off
 figure()
-sys = feedback(k * Kc * Gc * Gl * GH,1);
+sys = feedback(P * Kc * Gc * Gl * GHs,1);
 
 % show unit step plot
 step(sys)
@@ -67,4 +70,4 @@ title('Response to Unit Ramp Input')
 sse = abs(1-dcgain(sys));
 fprintf('The sse is %f\n',sse);
 
-tf(k * Kc * Gc * Gl * GH,1)
+tf(P * Kc * Gc * Gl * GHs,1)
